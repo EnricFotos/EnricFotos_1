b7b8e4418e46584761a04953beef8b1aa80e8775
EnricFotos <etorresbcn@gmail.com>
2024-10-20 20:04:38 +0800

    Add files via upload

---
+++ scripts/jQuery.js
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+	    <link rel="shortcut icon" href="https://www.ericlafforgue.com/files/shares/favicons/favicon.ico">
+	    <link rel="apple-touch-icon" sizes="57x57" href="https://www.ericlafforgue.com/files/shares/favicons/apple-touch-icon-57x57.png">
+	    <link rel="apple-touch-icon" sizes="114x114" href="https://www.ericlafforgue.com/files/shares/favicons/apple-touch-icon-114x114.png">
+	    <link rel="apple-touch-icon" sizes="72x72" href="https://www.ericlafforgue.com/files/shares/favicons/apple-touch-icon-72x72.png">
+	    <link rel="apple-touch-icon" sizes="144x144" href="https://www.ericlafforgue.com/files/shares/favicons/apple-touch-icon-144x144.png">
+	    <link rel="apple-touch-icon" sizes="60x60" href="https://www.ericlafforgue.com/files/shares/favicons/apple-touch-icon-60x60.png">
+	    <link rel="apple-touch-icon" sizes="120x120" href="https://www.ericlafforgue.com/files/shares/favicons/apple-touch-icon-120x120.png">
+	    <link rel="apple-touch-icon" sizes="76x76" href="https://www.ericlafforgue.com/files/shares/favicons/apple-touch-icon-76x76.png">
+	    <link rel="apple-touch-icon" sizes="152x152" href="https://www.ericlafforgue.com/files/shares/favicons/apple-touch-icon-152x152.png">
+	    <link rel="icon" type="image/png" href="https://www.ericlafforgue.com/files/shares/favicons/favicon-196x196.png" sizes="196x196">
+	    <link rel="icon" type="image/png" href="https://www.ericlafforgue.com/files/shares/favicons/favicon-160x160.png" sizes="160x160">
+	    <link rel="icon" type="image/png" href="https://www.ericlafforgue.com/files/shares/favicons/favicon-96x96.png" sizes="96x96">
+	    <link rel="icon" type="image/png" href="https://www.ericlafforgue.com/files/shares/favicons/favicon-16x16.png" sizes="16x16">
+	    <link rel="icon" type="image/png" href="https://www.ericlafforgue.com/files/shares/favicons/favicon-32x32.png" sizes="32x32">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="csrf-token" content="1VVhRLRtk8kom00AYs9iyT7b7YtCBlf5AxmfjYkv">
+        <title>ERIC LAFFORGUE PHOTOGRAPHY</title>
+        <meta name="description" content="Ever since he was young, he has been fascinated by far away countries and travelling, and spent some time in Africa, unknowingly retracing Monfried and Joseph Kessel’s footsteps when he was 10 years old in Djibouti, Ethiopia and Yemen.">
+        <meta name="keywords" content="">
+        <meta name="author" content="OsiRisK Creations">
+        
+        
+        <link media="all" type="text/css" rel="stylesheet" href="https://www.ericlafforgue.com/css/frontend.css?id=fc5fae28188c49be3488">
+
+
+        	<link rel="stylesheet" href="https://www.ericlafforgue.com/css/panier.css">
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-lg main-menu">
+    <a href="https://www.ericlafforgue.com" class="navbar-brand"><img src="https://www.ericlafforgue.com/img/logo_ericlafforguephotography.png" alt="ERIC LAFFORGUE PHOTOGRAPHY" /></a>
+
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation">
+        <span class="navbar-toggler-icon"><i class="fa fa-bars"></i></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+	    <div class="d-lg-none sm-sidebar">
+			   
+			<div id="searchBox" class="d-lg-none searchBox" data-open="false">
+				<form method="GET" action="https://www.ericlafforgue.com/search" accept-charset="UTF-8">
+					<input id="search" placeholder="Search photo" class="search d-none" name="search" type="text" value="">
+					<i class='fa fa-search'></i>
+				</form>
+			</div>
+	    </div>
+        <ul class="navbar-nav">
+                        	            	<li class="nav-item align-self-center">
+            			            		            				            															            		<a href="https://www.ericlafforgue.com/articles/Art%20Prints" class="nav-link ">Art Prints</a>
+	            		            </li>
+	                                    	            	<li class="nav-item align-self-center">
+            			            		            				            															            		<a href="https://www.ericlafforgue.com/gallery/Countries" class="nav-link ">Countries</a>
+	            		            </li>
+	                                    	            	<li class="nav-item align-self-center">
+            			            		            				            															            		<a href="https://www.ericlafforgue.com/gallery/Places" class="nav-link ">Places</a>
+	            		            </li>
+	                                    	            	<li class="nav-item align-self-center">
+            			            		            				            															            		<a href="https://www.ericlafforgue.com/gallery/Thema" class="nav-link ">Thema</a>
+	            		            </li>
+	                                    	            	<li class="nav-item align-self-center">
+            			            		            				            															            		<a href="https://www.ericlafforgue.com/articles/Stories" class="nav-link ">Stories</a>
+	            		            </li>
+	                                    	            	<li class="nav-item align-self-center">
+            			            		            				            															            		<a href="https://www.ericlafforgue.com/gallery/People" class="nav-link ">People</a>
+	            		            </li>
+	                                    	            	<li class="nav-item align-self-center">
+            			            		            				            															            		<a href="https://www.ericlafforgue.com/articles/Videos" class="nav-link ">Videos</a>
+	            		            </li>
+	                                    	            	<li class="nav-item align-self-center">
+            			            		            				            															            		<a href="https://www.ericlafforgue.com/articles/Tearsheets" class="nav-link ">Tearsheets</a>
+	            		            </li>
+	                                    	                        	            	<li class="nav-item align-self-center">
+            			            		            				            															            		<a href="https://www.ericlafforgue.com/articles/About" class="nav-link ">About</a>
+	            		            </li>
+	                                    	                        
+                    </ul>
+    </div>
+</nav>
+            <div class="d-block sidebar">
+				<div id="searchBox" class="d-none d-lg-block searchBox side" data-open="false">
+	<form method="GET" action="https://www.ericlafforgue.com/search" accept-charset="UTF-8">
+	    <input id="search" placeholder="Search photo" class="search d-none" name="search" type="text" value="">
+	    <i class='fa fa-search'></i>
+	</form>
+</div>
+<a href="https://www.ericlafforgue.com/cart/order-summary" id="panierLink" data-open="false">
+	<i class='fa fa-shopping-cart'>
+				<span class="badge badge-pill badge-warning d-none">0</span>
+	</i>
+	<span class="label">My cart</span>
+</a>
+<a href="https://www.ericlafforgue.com/bookmarks" id="favorisLink" data-open="false" class="favorisLink d-none">
+	<i class='fa fa-star'></i>
+	<span class="label">My bookmarks</span>
+</a>
+            </div>
+			
+			            <div class="container p-0 pt-5 pb-5 text-center paiement-retour">
+	<h1>Error 404</h1>
+	<h2>Sorry, the page you are looking for could not be found.</h2>
+</div>
+            
+            <footer>
+	<div class="footer-head d-flex">
+		<div class="footer-brand d-none d-sm-inline-block align-self-center"><img src="https://www.ericlafforgue.com/img/logo_footer_ericlafforguephotography.png" alt="ERIC LAFFORGUE PHOTOGRAPHY" /></div>
+		<ul class="footer-social ml-sm-auto align-self-center">
+			<li><a href="https://twitter.com/ericlafforgue" target="_blank" title="Tiwtter"><i class="fa fa-twitter-square" alt="Twitter"></i></a></li>
+			<li><a href="https://www.facebook.com/lafforguephotographer/" target="_blank" title="Facebook"><i class="fa fa-facebook-square" alt="Facebook"></i></a></li>
+			<li><a href="https://www.linkedin.com/in/lafforgue" target="_blank" title="Linkedin"><i class="fa fa-linkedin" alt="Linkedin"></i></a></li>
+			<li><a href="https://www.instagram.com/ericlafforgue/" target="_blank" title="Instagram"><i class="fa fa-instagram" alt="Instagram"></i></a></li>
+			<li><a href="https://www.flickr.com/photos/mytripsmypics/albums/" target="_blank" title="Flickr"><i class="fa fa-flickr" alt="Flickr"></i></a></li>
+		</ul>
+	</div>
+	<div class="footer-body">
+		<ul class="contact">
+			<li><a href="mailto:lafforgue@mac.com" target="_blank"><i class="far fa-envelope"></i> lafforgue@mac.com</a></li>
+			<li><a href="tel:+33.633.05.34.51" target="_blank"><i class="fas fa-mobile-alt"></i> +33.633.05.34.51</a></li>
+		</ul>
+		<div class="footer-menu">
+			            	            			            		            				            															            		<a href="https://www.ericlafforgue.com/articles/Art%20Prints">Art Prints</a>
+	            		            		            		&nbsp;| 
+	            		                                    	            			            		            																						            		<a href="https://www.ericlafforgue.com/gallery/Countries">Countries</a>
+	            		            		            		&nbsp;| 
+	            		                                    	            			            		            																						            		<a href="https://www.ericlafforgue.com/gallery/Places">Places</a>
+	            		            		            		&nbsp;| 
+	            		                                    	            			            		            																						            		<a href="https://www.ericlafforgue.com/gallery/Thema">Thema</a>
+	            		            		            		&nbsp;| 
+	            		                                    	            			            		            				            															            		<a href="https://www.ericlafforgue.com/articles/Stories">Stories</a>
+	            		            		            		&nbsp;| 
+	            		                                    	            			            		            																						            		<a href="https://www.ericlafforgue.com/gallery/People">People</a>
+	            		            		            		&nbsp;| 
+	            		                                    	                        	            			            		            				            															            		<a href="https://www.ericlafforgue.com/articles/Tearsheets">Tearsheets</a>
+	            		            		            		&nbsp;| 
+	            		                                    	            			            		            				            															            		<a href="https://www.ericlafforgue.com/articles/Books">Books</a>
+	            		            		            		&nbsp;| 
+	            		                                    	            			            		            				            															            		<a href="https://www.ericlafforgue.com/articles/About">About</a>
+	            		            		            		&nbsp;| 
+	            		                                    	            			            		            				            															            		<a href="https://www.ericlafforgue.com/articles/Terms%20of%20use">Terms of use</a>
+	            		            		                        		</div>
+	</div>
+</footer>        </div><!-- #app -->
+        <div id="loading-search">
+	        <img src="https://www.ericlafforgue.com/img/loading.png" />
+	        <span>SEARCHING</span>
+	        Please wait...
+        </div>
+
+        <!-- Scripts -->
+                <script src="https://www.ericlafforgue.com/js/frontend.js?id=ecaef213238087274e6a"></script>
+
+        <script src="https://www.ericlafforgue.com/js/toastr.js"></script>
+
+        		
+		    <div class="js-cookie-consent cookie-consent">
+	<div class="container text-center">
+	    <span class="cookie-consent__message">
+	        By continuing your visit to this site, you accept the use of Cookies for performing statistical visits and propose sharing buttons. <a href="https://www.ericlafforgue.com/articles/Policy">Learn more</a>
+	    </span>
+	
+	    <button class="js-cookie-consent-agree cookie-consent__agree btn ml-3">
+	        Allow cookies
+	    </button>
+	</div>
+</div>
+
+    <script>
+
+        window.laravelCookieConsent = (function () {
+
+            var COOKIE_VALUE = 1;
+
+            function consentWithCookies() {
+                setCookie('laravel_cookie_consent', COOKIE_VALUE, 390);
+                hideCookieDialog();
+            }
+
+            function cookieExists(name) {
+                return (document.cookie.split('; ').indexOf(name + '=' + COOKIE_VALUE) !== -1);
+            }
+
+            function hideCookieDialog() {
+                var dialogs = document.getElementsByClassName('js-cookie-consent');
+
+                for (var i = 0; i < dialogs.length; ++i) {
+                    dialogs[i].style.display = 'none';
+                }
+            }
+
+            function setCookie(name, value, expirationInDays) {
+                var date = new Date();
+                date.setTime(date.getTime() + (expirationInDays * 24 * 60 * 60 * 1000));
+                document.cookie = name + '=' + value + '; ' + 'expires=' + date.toUTCString() +';path=/';
+            }
+
+            if(cookieExists('laravel_cookie_consent')) {
+                hideCookieDialog();
+            }
+
+            var buttons = document.getElementsByClassName('js-cookie-consent-agree');
+
+            for (var i = 0; i < buttons.length; ++i) {
+                buttons[i].addEventListener('click', consentWithCookies);
+            }
+
+            return {
+                consentWithCookies: consentWithCookies,
+                hideCookieDialog: hideCookieDialog
+            };
+        })();
+    </script>
+
+</body>
+</html>
